3f9f387eddb41070d3da5ca76ae3c74e4a15e8bd
jellie <talentforest0501@gmail.com>
2024-08-24 16:58:21 +0900

    design: tailwind.config.js 설정과 폰트 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,33 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      screens: {
+        sm: { max: '768px' },
+      },
       fontFamily: {
         pretendard: ['Pretendard', 'sans-serif'],
+      },
+      colors: {
+        bg: '#F5F5F7',
+        text: '#2b2b2b',
+
+        gray1: '#6d6d6d',
+        gray2: '#b5b5b5',
+        gray3: '#dedede',
+        gray4: '#e9e9e9',
+
+        white: '#fff',
+
+        oliveGreen1: '#6fa235',
+        oliveGreen2: '#b4d780',
+        oliveGreen3: '#cae3a5',
+
+        vividGreen1: '#00ad49',
+        vividGreen2: '#69c57e',
+        vividGreen3: '#c0e5c7',
+
+        skyBlue1: '#00b2e3',
+        skyBlue2: '#7bd2ec',
       },
     },
   },
