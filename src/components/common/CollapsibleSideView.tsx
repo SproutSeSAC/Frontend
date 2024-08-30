9f1b0d0820177d72280b691ce08fb73d341d018b
김민준 <alswns1285@gmail.com>
2024-08-30 14:11:28 +0900

    feat: CollapsibleSideView 공통 컴포넌트 구현

---
+++ src/components/common/CollapsibleSideView.tsx
@@ -0,0 +1,31 @@
+interface CollapsibleSideViewProps {
+  sideViewOpen: boolean;
+  onClose: () => void;
+  headerContent: React.ReactNode;
+  mainContent: React.ReactNode;
+}
+
+export default function CollapsibleSideView({
+  sideViewOpen,
+  onClose,
+  headerContent,
+  mainContent,
+}: CollapsibleSideViewProps) {
+  return (
+    <section
+      className={`transtition-all z-10 h-full duration-200 ease-in-out ${sideViewOpen ? 'w-[27%] translate-x-0' : 'w-0 translate-x-full'} mr-5 max-w-[284px] py-11`}
+    >
+      {sideViewOpen && (
+        <header className="mb-10 mt-[50px] flex justify-between font-semibold text-gray2">
+          <button type="button" className="mt-auto text-sm" onClick={onClose}>
+            접어두기
+          </button>
+
+          {headerContent}
+        </header>
+      )}
+
+      {mainContent}
+    </section>
+  );
+}
--- src/components/meal-recruit/MealRecruitSideView.tsx
+++ src/components/meal-recruit/MealRecruitSideView.tsx
@@ -1,36 +1,30 @@
+import CollapsibleSideView from '@/components/common/CollapsibleSideView';
 import MealRecruitSlider from '@/components/meal-recruit/MealRecruitSlider';
 
 interface MealRecruitSideViewProps {
   sideViewOpen: boolean;
-  setSideViewOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  onClose: () => void;
 }
 
 export default function MealRecruitSideView({
   sideViewOpen,
-  setSideViewOpen,
+  onClose,
 }: MealRecruitSideViewProps) {
+  const headerContent = (
+    <span className="text-[27px]">
+      <span>오늘의 </span>
+      <span className="text-vividGreen1">한끼팟!</span>
+    </span>
+  );
+
+  const mainContent = <MealRecruitSlider sideViewOpen={sideViewOpen} />;
+
   return (
-    <section
-      className={`transtition-all z-10 h-full duration-200 ease-in-out ${sideViewOpen ? 'w-[27%] translate-x-0' : 'w-0 translate-x-full'} mr-5 max-w-[284px] py-11`}
-    >
-      {sideViewOpen && (
-        <header className="mb-10 mt-[50px] flex justify-between font-semibold text-gray2">
-          <button
-            type="button"
-            className="mt-auto text-sm"
-            onClick={() => setSideViewOpen(false)}
-          >
-            접어두기
-          </button>
-
-          <span className="text-[27px]">
-            <span>오늘의 </span>
-            <span className="text-vividGreen1">한끼팟!</span>
-          </span>
-        </header>
-      )}
-
-      <MealRecruitSlider sideViewOpen={sideViewOpen} />
-    </section>
+    <CollapsibleSideView
+      sideViewOpen={sideViewOpen}
+      onClose={onClose}
+      headerContent={headerContent}
+      mainContent={mainContent}
+    />
   );
 }
