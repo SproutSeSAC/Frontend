864e91ae7d27d31b2b7bcdbd14bedac57d205337
김민준 <alswns1285@gmail.com>
2024-09-02 00:19:58 +0900

    fix: width 변화 시 지도 짤림 현상 해결

--- src/hooks/useCollapsibleSideView.ts
+++ src/hooks/useCollapsibleSideView.ts
@@ -1,7 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function useCollapsibleSideView() {
   const [sideViewOpen, setSideViewOpen] = useState(true);
+
+  // sideView toggle로 인해서 지도의 부모 컴포넌트의 width가 변하게 됩니다.
+  // 이런 경우에 지도는 첫 렌더링때 불러왔던 width값과, center값 등 위치값이 달라지기 때문에,
+  // resize 이벤트를 걸어주어 지도 짤림 현상과 center가 맞지 않는 현상을 방지합니다.
+  useEffect(() => {
+    setTimeout(() => {
+      window.dispatchEvent(new Event('resize'));
+    }, 100);
+  }, [sideViewOpen]);
 
   const openSideView = () => setSideViewOpen(true);
   const closeSideView = () => setSideViewOpen(false);
