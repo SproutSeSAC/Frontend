96ff88e4bf91f56f87cf2cff1b41e890e310aa21
jellie <talentforest0501@gmail.com>
2024-09-19 16:12:29 +0900

    feat: full-calendar 라이브러리 설치

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,11 +1,15 @@
 import Header from '@/layouts/Header';
 import MainView from '@/layouts/MainView';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import FullCalendar from '@fullcalendar/react';
 
 export default function Schedule() {
   return (
     <MainView>
       <Header title="김철수 님의 이번달도 응원합니다!" highlight="김철수" />
-      <div className="">일정관리페이지</div>
+      <div className="">
+        <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+      </div>
     </MainView>
   );
 }
