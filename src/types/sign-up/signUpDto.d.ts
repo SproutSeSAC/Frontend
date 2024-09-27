355a91544e7129f8ac8d7332308093ca26e1bf68
jellie <talentforest0501@gmail.com>
2024-09-27 16:27:21 +0900

    rename: type 폴더

---
+++ src/types/sign-up/signUpDto.d.ts
@@ -0,0 +1,26 @@
+export type SignUpFormTitle = {
+  title: { text: string; condition?: string };
+  additionalInfo?: string;
+};
+
+export type SignUpFormData = {
+  step: number;
+  roles: KeyOfRole[];
+  name: string;
+  nickname: string;
+  campusId: number[];
+  courseId: number[];
+  jobIdList: number[];
+  domainIdList: number[];
+  techStackIdList: number[];
+  verifyCode: string;
+  marketingConsent: [true, false];
+};
+
+export type SignUpQuestionsByStep = {
+  [K in keyof SignUpFormData]: SignUpFormTitle & Pick<SignUpFormData, K>;
+}[keyof SignUpFormData];
+
+export type VerifyCode = {
+  verifyCode: string;
+};
