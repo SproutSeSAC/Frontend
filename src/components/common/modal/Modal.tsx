3f00893e05dd1493b23e05dbe2dd28f5baa0f83e
jellie <talentforest0501@gmail.com>
2024-10-25 00:29:49 +0900

    design: 회원탈퇴 모달 UI

--- src/components/common/modal/Modal.tsx
+++ src/components/common/modal/Modal.tsx
@@ -3,12 +3,14 @@ import { ReactNode, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 import { BsX } from 'react-icons/bs';
+import { FaChevronLeft } from 'react-icons/fa6';
 
 interface Props {
   onToggleClick: () => void;
   title: string | ReactNode;
   children: ReactNode;
   className?: string;
+  headerType?: 'title-xIcon' | 'squareBackBtn-title';
 }
 
 export default function Modal({
@@ -16,6 +18,7 @@ export default function Modal({
   title,
   children,
   className,
+  headerType = 'title-xIcon',
 }: Props) {
   const el = document.getElementById('modal') as Element;
 
@@ -31,12 +34,34 @@ export default function Modal({
       <section
         className={`fixed inset-0 z-40 m-auto h-fit max-h-[80vh] min-h-[180px] w-fit min-w-[350px] overflow-hidden rounded-2xl bg-white ${className}`}
       >
-        <header className="flex items-start justify-between p-3">
-          <h2 className="text-xl font-semibold">{title}</h2>
-          <button type="button" aria-label="모달 닫기" onClick={onToggleClick}>
-            <BsX size={30} />
-          </button>
-        </header>
+        {headerType === 'title-xIcon' && (
+          <header className="flex items-start justify-between p-3">
+            <h2 className="text-xl font-semibold">{title}</h2>
+            <button
+              type="button"
+              aria-label="모달 닫기"
+              onClick={onToggleClick}
+            >
+              <BsX size={30} />
+            </button>
+          </header>
+        )}
+
+        {headerType === 'squareBackBtn-title' && (
+          <header className="flex items-center justify-between p-3">
+            <button
+              type="button"
+              aria-label="접어두기"
+              className="flex aspect-square h-10 items-center justify-center rounded-lg border border-gray2 bg-white text-sm text-gray1"
+              onClick={onToggleClick}
+            >
+              <FaChevronLeft />
+            </button>
+            <h2 className="mr-10 w-full text-center text-lg font-semibold">
+              {title}
+            </h2>
+          </header>
+        )}
 
         <div className="max-h-[58vh] overflow-scroll p-3 scrollbar-hide">
           {children}
