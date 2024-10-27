c284f6e7c154d1c40ec440d62edc9a4a742eebb6
jellie <talentforest0501@gmail.com>
2024-10-27 17:06:39 +0900

    chore: 네비게이션바 설정 버튼 삭제

--- src/layouts/NavigationBar.tsx
+++ src/layouts/NavigationBar.tsx
@@ -2,10 +2,7 @@ import { Link, useLocation } from 'react-router-dom';
 
 import StoreIcon from '@/assets/icons/store.svg?react';
 import Logo from '@/layouts/Logo';
-import { BsCalendar, BsGear, BsHouse, BsPersonFillAdd } from 'react-icons/bs';
-import { FaChevronRight } from 'react-icons/fa6';
-
-import ToggleButton from '@/components/common/button/ToggleButton';
+import { BsCalendar, BsHouse, BsPersonFillAdd } from 'react-icons/bs';
 
 export default function NavigationBar() {
   const { pathname } = useLocation();
@@ -35,10 +32,6 @@ export default function NavigationBar() {
     },
   ];
 
-  const onLeaveClick = () => {
-    console.log('회원 탈퇴');
-  };
-
   return (
     <nav className="sticky top-0 flex h-[100vh] w-[100px] flex-col items-center justify-between px-5 pb-[4%] pt-11">
       <Logo />
@@ -58,38 +51,6 @@ export default function NavigationBar() {
           ))}
         </ul>
       </div>
-
-      {pathname === '/mypage' && (
-        <div className="group relative flex size-8 items-center justify-center">
-          <BsGear className="size-6 text-gray1" />
-          <div className="absolute bottom-0 left-4 hidden px-4 group-hover:block">
-            <ul className="w-32 rounded-md bg-white p-2 shadow-card">
-              {['알림', '회원탈퇴'].map(label => (
-                <li
-                  key={label}
-                  className="flex items-center justify-between gap-1 rounded-lg p-2 hover:bg-gray4"
-                >
-                  <span
-                    className={`font-semibold ${label === '회원탈퇴' && 'text-red-600'}`}
-                  >
-                    {label}
-                  </span>
-                  {label === '알림' && <ToggleButton />}
-                  {label === '회원탈퇴' && (
-                    <button
-                      className="px-1 py-2"
-                      type="button"
-                      onClick={onLeaveClick}
-                    >
-                      <FaChevronRight className="text-[13px] text-gray1" />
-                    </button>
-                  )}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      )}
     </nav>
   );
 }
