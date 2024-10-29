03000288c21ec7931abe1ec8a19eceb5c721bd1d
김현진 <kimhyunjin@gimhyeonjin-ui-noteubug.local>
2024-10-30 03:26:18 +0900

    feat: 맛집 상세위치페이지 수정 및 optionList.ts 생성

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ const router = createBrowserRouter([
         element: <Store />,
       },
       {
-        path: 'stores/:storeId',
+        path: 'stores/detail-location',
         element: <StoreDetail />,
       },
       {
--- src/constants/seviceConstant.ts
+++ src/constants/seviceConstant.ts
@@ -7,3 +7,52 @@ export type ProjectStatusType =
   | typeof STATUS_ACTIVE
   | typeof STATUS_INACTIVE
   | typeof STATUS_END;
+
+// 캠퍼스 정보
+export const CAMPUS_SEONGBUK = 'SEONGBUK';
+export const CAMPUS_DOBONG = 'DOBONG';
+export const CAMPUS_GANGBUK = 'GANGBUK';
+export const CAMPUS_DONGDAEMUN = 'DONGDAEMUN';
+
+export type CampusType =
+  | typeof CAMPUS_SEONGBUK
+  | typeof CAMPUS_DOBONG
+  | typeof CAMPUS_GANGBUK
+  | typeof CAMPUS_DONGDAEMUN;
+
+export const campusList: Array<{
+  id: number;
+  name: string;
+  key: CampusType;
+  latitude: string;
+  longitude: string;
+}> = [
+  {
+    id: 1,
+    name: '성북캠퍼스',
+    key: CAMPUS_SEONGBUK,
+    latitude: '37.6043803',
+    longitude: '127.0366509',
+  },
+  {
+    id: 2,
+    name: '도봉캠퍼스',
+    key: CAMPUS_DOBONG,
+    latitude: '37.6544068',
+    longitude: '127.0497957',
+  },
+  {
+    id: 3,
+    name: '강북캠퍼스',
+    key: CAMPUS_GANGBUK,
+    latitude: '37.6558908',
+    longitude: '127.0135631',
+  },
+  {
+    id: 4,
+    name: '동대문캠퍼스',
+    key: CAMPUS_DONGDAEMUN,
+    latitude: '37.5773150',
+    longitude: '127.0430027',
+  },
+];
