17b86b54c9ac9a0aa5519ec85f9536d5c8bcd337
jellie <talentforest0501@gmail.com>
2024-11-15 21:58:03 +0900

    design: 마이페이지 변경

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-
 import {
   initialUserProfile,
   useGetUserProfile,
@@ -59,12 +57,6 @@ export default function MyPage() {
       <section className="mb-16 w-full flex-1">
         <div className="mb-[10px] flex items-center justify-between">
           <Title title={`${name}님이 찜한 글 모음`} />
-          <Link
-            to="/lounge"
-            className="text-sm font-semibold tracking-tight text-gray1"
-          >
-            전체보기
-          </Link>
         </div>
 
         <ScrollContainer>
