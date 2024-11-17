41820b1ffefb4c21638789bcc9c01f59413aad30
김현진 <kimhyunjin@gimhyeonjin-ui-noteubug.local>
2024-11-17 21:53:27 +0900

    feat: 불필요한 div, ref삭제

--- src/components/common/CustomDatePicker.tsx
+++ src/components/common/CustomDatePicker.tsx
@@ -1,5 +1,3 @@
-import { useRef } from 'react';
-
 import ErrorMsg from './input/ErrorMsg';
 
 import DatePicker, { ReactDatePickerCustomHeaderProps } from 'react-datepicker';
@@ -37,89 +35,84 @@ export default function CustomDatePicker({
     '11',
     '12',
   ];
-  const datePickerRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div id="customDatePicker" ref={datePickerRef} className="relative w-full">
-      <div className="relative flex flex-col">
-        <DatePicker
-          placeholderText={placeholder}
-          className={
-            className
-              ? `w-full bg-white outline-none placeholder:text-gray2 ${className}`
-              : 'w-full bg-white p-4 placeholder:text-gray2'
-          }
-          renderCustomHeader={({
-            date,
-            changeYear,
-            changeMonth,
-            decreaseMonth,
-            increaseMonth,
-            prevMonthButtonDisabled,
-            nextMonthButtonDisabled,
-          }: ReactDatePickerCustomHeaderProps) => (
-            <div className="text-md my-1.5 flex justify-center gap-2">
-              <button
-                className="border-none bg-none"
-                type="button"
-                onClick={decreaseMonth}
-                disabled={prevMonthButtonDisabled}
+    <div className="relative flex w-full flex-col" id="customDatePicker">
+      <DatePicker
+        placeholderText={placeholder}
+        className={
+          className
+            ? `w-full bg-white outline-none placeholder:text-gray2 ${className}`
+            : 'w-full bg-white p-4 placeholder:text-gray2'
+        }
+        renderCustomHeader={({
+          date,
+          changeYear,
+          changeMonth,
+          decreaseMonth,
+          increaseMonth,
+          prevMonthButtonDisabled,
+          nextMonthButtonDisabled,
+        }: ReactDatePickerCustomHeaderProps) => (
+          <div className="text-md my-1.5 flex justify-center gap-2">
+            <button
+              className="border-none bg-none"
+              type="button"
+              onClick={decreaseMonth}
+              disabled={prevMonthButtonDisabled}
+            >
+              <BsFillCaretLeftFill />
+            </button>
+            <div className="w-26 flex items-center justify-between">
+              <select
+                className="mr-0.5 rounded-sm text-text"
+                value={date.getFullYear()}
+                onChange={({ target: { value } }) => changeYear(Number(value))}
               >
-                <BsFillCaretLeftFill />
-              </button>
-              <div className="w-26 flex items-center justify-between">
-                <select
-                  className="mr-0.5 rounded-sm text-text"
-                  value={date.getFullYear()}
-                  onChange={({ target: { value } }) =>
-                    changeYear(Number(value))
-                  }
-                >
-                  {years.map(option => (
-                    <option key={option} value={option}>
-                      {option}
-                    </option>
-                  ))}
-                </select>
-                <span>년</span>
-              </div>
-              <div className="w-26 flex items-center justify-between">
-                <select
-                  className="mr-0.5 rounded-sm text-text"
-                  value={months[date.getMonth()]}
-                  onChange={({ target: { value } }) =>
-                    changeMonth(months.indexOf(value))
-                  }
-                >
-                  {months.map(option => (
-                    <option key={option} value={option}>
-                      {option}
-                    </option>
-                  ))}
-                </select>
-                <span>월</span>
-              </div>
-              <button
-                type="button"
-                onClick={increaseMonth}
-                disabled={nextMonthButtonDisabled}
-              >
-                <BsFillCaretRightFill />
-              </button>
+                {years.map(option => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+              <span>년</span>
             </div>
-          )}
-          dateFormat="yyyy.MM.dd"
-          selected={currentDate}
-          onChange={onChange}
-          selectsStart
-        />
-        {errorMsg && (
-          <ErrorMsg
-            msg={errorMsg || ''}
-            className="absolute bottom-[-26px] ml-2"
-          />
+            <div className="w-26 flex items-center justify-between">
+              <select
+                className="mr-0.5 rounded-sm text-text"
+                value={months[date.getMonth()]}
+                onChange={({ target: { value } }) =>
+                  changeMonth(months.indexOf(value))
+                }
+              >
+                {months.map(option => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+              <span>월</span>
+            </div>
+            <button
+              type="button"
+              onClick={increaseMonth}
+              disabled={nextMonthButtonDisabled}
+            >
+              <BsFillCaretRightFill />
+            </button>
+          </div>
         )}
-      </div>
+        dateFormat="yyyy.MM.dd"
+        selected={currentDate}
+        onChange={onChange}
+        selectsStart
+      />
+      {errorMsg && (
+        <ErrorMsg
+          msg={errorMsg || ''}
+          className="absolute bottom-[-26px] ml-2"
+        />
+      )}
     </div>
   );
 }
