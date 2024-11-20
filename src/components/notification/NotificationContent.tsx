afeac15f047cfbd9311b752aa9fcc7ee985089ee
김현진 <kimhyunjin@gimhyeonjin-ui-noteubug.local>
2024-11-20 21:16:20 +0900

    feat: 알림센터 useblocker 제거 및 파일 분리

---
+++ src/components/notification/NotificationContent.tsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+
+import CardContent from './CardContent';
+
+export default function NotificationContent() {
+  return (
+    <div className="flex h-[calc(100vh-24px-52px-40px)] flex-col gap-6 overflow-y-scroll">
+      {[1, 2, 3, 4, 5].map(item => {
+        return (
+          <div key={item} className="w-full">
+            {item < 3 ? (
+              <Link
+                to="/lounge/post/1"
+                className={`block rounded-lg border border-solid border-gray4 p-4 text-sm text-gray1 ${item === 5 ? 'mb-10' : ''}`}
+              >
+                <CardContent item={item} />
+              </Link>
+            ) : (
+              <div
+                className={`rounded-lg border border-solid border-gray4 bg-gray3 p-4 text-sm text-gray1 ${item === 5 ? 'mb-10' : ''}`}
+              >
+                <CardContent item={item} disabled />
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
---
+++ src/components/notification/NotificationSideView.tsx
@@ -0,0 +1,45 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+import { useLocation } from 'react-router-dom';
+
+import { notificationOpenAtom } from '@/atoms/notificationAtom';
+
+import NotificationContent from './NotificationContent';
+import NotificationHeader from './NotificationHeader';
+
+import { useAtom } from 'jotai';
+
+import CollapsibleSideView from '@/components/common/container/CollapsibleSideView';
+import OutsideClickContainer from '@/components/common/container/OutsideClickContainer';
+
+export default function NotificationSideView() {
+  const location = useLocation();
+  const previousPath = useRef(location.pathname);
+
+  const [isNotificationOpen, setIsNotificationOpen] =
+    useAtom(notificationOpenAtom);
+
+  const handleClose = useCallback(() => {
+    setIsNotificationOpen(false);
+  }, [setIsNotificationOpen]);
+
+  useEffect(() => {
+    if (isNotificationOpen && previousPath.current !== location.pathname) {
+      handleClose();
+    }
+    previousPath.current = location.pathname;
+  }, [handleClose, isNotificationOpen, location.pathname]);
+
+  return (
+    <OutsideClickContainer onClose={handleClose}>
+      <CollapsibleSideView
+        sideViewOpen={isNotificationOpen}
+        onClose={handleClose}
+        headerContent={<NotificationHeader handleClose={handleClose} />}
+        mainContent={<NotificationContent />}
+        className="fixed right-0 max-w-[412px] bg-white px-5 pt-10 shadow-card"
+        hideButton
+      />
+    </OutsideClickContainer>
+  );
+}
