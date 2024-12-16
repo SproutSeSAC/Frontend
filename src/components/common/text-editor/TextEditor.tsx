70dfe5b3a2a91ee8607ef4da9572721694799dab
jellie <talentforest0501@gmail.com>
2024-12-17 00:54:35 +0900

    feat: 공지사항 이미지 용량 1MB 제한

--- src/components/common/text-editor/TextEditor.tsx
+++ src/components/common/text-editor/TextEditor.tsx
@@ -2,8 +2,12 @@ import { useCallback, useEffect, useMemo, useRef } from 'react';
 
 import EmojiTool from './EmojiTool';
 
+import { useDialogContext } from '@/hooks';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
+import Toolbar from 'quill/modules/toolbar';
+
+import SquareButton from '@/components/common/button/SquareButton';
 
 interface TextEditorProps {
   onChange: (value: string) => void;
@@ -18,6 +22,8 @@ export default function TextEditor({
 }: TextEditorProps) {
   const editorRef = useRef<HTMLDivElement | null>(null);
   const quillRef = useRef<Quill | null>(null);
+
+  const { alert, hideDialog } = useDialogContext();
 
   const handleChange = useCallback(() => {
     if (quillRef.current) {
@@ -52,6 +58,41 @@ export default function TextEditor({
     ];
   }, []);
 
+  const handleImageUpload = useCallback(() => {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+
+    input.onchange = async () => {
+      const file = input.files?.[0];
+      if (file) {
+        if (file.size > 1048576) {
+          alert({
+            text: '이미지 크기는 1MB 이하만 업로드 가능합니다.',
+            children: (
+              <SquareButton name="확인" onClick={hideDialog} type="button" />
+            ),
+          });
+          return;
+        }
+        const reader = new FileReader();
+        reader.onload = () => {
+          if (quillRef.current) {
+            const range = quillRef.current.getSelection();
+            const base64String = reader.result as string;
+            quillRef.current.insertEmbed(
+              range?.index || 0,
+              'image',
+              base64String,
+            );
+          }
+        };
+        reader.readAsDataURL(file);
+      }
+    };
+  }, [alert, hideDialog]);
+
   useEffect(() => {
     if (editorRef.current && !quillRef.current) {
       quillRef.current = new Quill(editorRef.current, {
@@ -61,10 +102,18 @@ export default function TextEditor({
         },
         placeholder: placeholder || '',
       });
-
+      const toolbar = quillRef.current.getModule('toolbar') as Toolbar;
+      toolbar.addHandler('image', handleImageUpload);
+      toolbar.addHandler('emoji', handleEmojiSelect);
       quillRef.current.on('text-change', handleChange);
     }
-  }, [handleChange, handleEmojiSelect, placeholder, toolbarOptions]);
+  }, [
+    handleChange,
+    handleEmojiSelect,
+    handleImageUpload,
+    placeholder,
+    toolbarOptions,
+  ]);
 
   useEffect(() => {
     if (quillRef.current && value !== undefined) {
