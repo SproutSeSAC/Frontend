71564353672eb86a190f7d363040cbaf282e2a97
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-01-06 00:12:33 +0900

    feat: ErrorPage 컴포넌트에 레이아웃 추가 및 에러바운더리 상위에서만 관리하도록 수정

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,14 +1,20 @@
+import { ReactNode } from 'react';
+
 import { Outlet } from 'react-router-dom';
 
 import NavigationBar from '@/layouts/NavigationBar';
 
 import NotificationSideView from '@/components/notification/NotificationSideView';
 
-export default function Layout() {
+interface LayoutProps {
+  children?: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex min-h-[100vh] min-w-[1200px] bg-bg">
       <NavigationBar />
-      <Outlet />
+      {children || <Outlet />}
       <NotificationSideView />
     </div>
   );
