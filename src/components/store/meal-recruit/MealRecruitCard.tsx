cb06f1050c0d6e885aa6343bab9b145d9cf70586
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-01-14 19:02:34 +0900

    fix: 한끼팟 생성 시 약속시간 타입을 date에서 string으로 수정 및 utc 시간을 로컬시간으로 변환하는 코드 제거

--- src/components/store/meal-recruit/MealRecruitCard.tsx
+++ src/components/store/meal-recruit/MealRecruitCard.tsx
@@ -2,7 +2,7 @@ import { useCallback } from 'react';
 
 import { useDialogContext } from '@/hooks';
 import { MealPosts } from '@/types/store/storeMealPostDto';
-import { convertUtcToLocal, formatDate } from '@/utils';
+import { formatDate } from '@/utils';
 
 import MealRecruitCardModal from '@/components/store/meal-recruit/MealRecruitCardModal';
 import UserImage from '@/components/user/UserImage';
@@ -46,7 +46,7 @@ export default function MealRecruitCard({
             </span>
             <span className={`${isDisabled && 'text-gray1'}`}>
               {slideItem
-                ? `${formatDate(convertUtcToLocal(slideItem.appointmentTime), 'yyyy.MM.dd a h시 mm분')}`
+                ? `${formatDate(slideItem.appointmentTime, 'yyyy.MM.dd a h시 mm분')}`
                 : '-'}
             </span>
           </p>
