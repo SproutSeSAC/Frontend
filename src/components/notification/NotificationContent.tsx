02965db6a4dd18a816972041a5acc047e5245750
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-01-24 16:52:56 +0900

    feat: 알림 센터 사이드뷰 열릴때마다 알림데이터 받아오도록 refetch 추가

--- src/components/notification/NotificationContent.tsx
+++ src/components/notification/NotificationContent.tsx
@@ -1,36 +1,50 @@
+import { useEffect } from 'react';
+
 import { useNotification } from '@/hooks/notification/useNotification';
 
 import { useGetNotifications } from '@/services/notification/notificationQueries';
 
+import { notificationOpenAtom } from '@/atoms/notificationAtom';
+
+import { useAtomValue } from 'jotai';
 import { FaRegTrashAlt } from 'react-icons/fa';
 
 import CardContent from '@/components/notification/CardContent';
 
 export default function NotificationContent() {
-  const { data: notifications } = useGetNotifications();
+  const isNotificationOpen = useAtomValue(notificationOpenAtom);
+  const { data: notifications, refetch } = useGetNotifications();
   const { setNotificationAsRead, deleteAllNotification } = useNotification();
 
+  useEffect(() => {
+    if (isNotificationOpen) {
+      refetch();
+    }
+  }, [isNotificationOpen, refetch]);
+
   return (
-    <div className="flex h-full flex-col gap-6 overflow-y-auto">
+    <>
       <button
-        onClick={() => deleteAllNotification()}
-        className="flex items-center justify-end gap-1 text-end text-base text-[#6D6D6D]"
+        onClick={deleteAllNotification}
+        className="mb-6 flex items-center justify-end gap-1 text-end text-base text-[#6D6D6D]"
       >
         <FaRegTrashAlt />
         모두 삭제
       </button>
 
-      {notifications?.map(item => {
-        return (
-          <button
-            onClick={() => setNotificationAsRead(item.id)}
-            key={item.id}
-            className={`border-lightGrey block w-full rounded-lg border border-solid p-4 text-sm text-darkGray-active ${item.isRead && 'bg-mainGray'} cursor-pointer`}
-          >
-            <CardContent notification={item} />
-          </button>
-        );
-      })}
-    </div>
+      <div className="flex h-full flex-col gap-6 overflow-y-auto">
+        {notifications?.map(item => {
+          return (
+            <button
+              onClick={() => setNotificationAsRead(item.id)}
+              key={item.id}
+              className={`border-lightGrey block w-full rounded-lg border border-solid p-4 text-sm text-darkGray-active ${item.isRead && 'bg-mainGray'} cursor-pointer`}
+            >
+              <CardContent notification={item} />
+            </button>
+          );
+        })}
+      </div>
+    </>
   );
 }
