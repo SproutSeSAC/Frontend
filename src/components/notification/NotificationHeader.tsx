2368589af0a7b7c929c86e2d2003fd81d7dc0224
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-02-16 12:55:32 +0900

    design: 알림 info 스타일 수정

--- src/components/notification/NotificationHeader.tsx
+++ src/components/notification/NotificationHeader.tsx
@@ -25,7 +25,7 @@ export default function NotificationHeader({
         </div>
         <div className="group relative flex cursor-pointer items-center justify-center rounded-lg bg-bg">
           <IoIosInformation className="size-8 text-xl text-darkGray" />
-          <span className="absolute right-0 top-14 hidden w-[185px] rounded-lg bg-black px-5 py-6 text-base leading-6 text-white opacity-70 group-hover:block">
+          <span className="absolute right-0 top-14 z-10 hidden w-[185px] rounded-lg bg-black px-5 py-6 text-base leading-6 text-white opacity-70 group-hover:block">
             알림은 최대 20개까지 <br />
             표시됩니다.
           </span>
