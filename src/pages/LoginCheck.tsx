1477fb0dadfceb2a016ea7d00a1510497a41ea99
jellie <talentforest0501@gmail.com>
2025-02-26 22:24:18 +0900

    fix: 로그인체크 캘린더토큰 삭제

--- src/pages/LoginCheck.tsx
+++ src/pages/LoginCheck.tsx
@@ -2,13 +2,9 @@ import { useEffect } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
-import { getCalendarToken, loginCheck } from '@/services/auth/authQueries';
+import { loginCheck } from '@/services/auth/authQueries';
 
-import {
-  ACCESS_TOKEN_KEY,
-  CALENDAR_TOKEN_KEY,
-  REFRESH_TOKEN_KEY,
-} from '@/constants';
+import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from '@/constants';
 import { useDialogContext } from '@/hooks';
 import { setCookie } from '@/utils';
 import axios from 'axios';
@@ -36,13 +32,6 @@ export default function LoginCheck() {
         const response = await loginCheck();
 
         if (response.status === 200) {
-          const calendarToken = await getCalendarToken();
-
-          if (calendarToken.status === 200) {
-            const newCalendarAccessToken = calendarToken.data.access_token;
-            setCookie(CALENDAR_TOKEN_KEY, newCalendarAccessToken, 1);
-          }
-
           navigate('/');
         }
       } catch (error) {
