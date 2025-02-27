f6eb0ee069022230b3f6dd21c79598d64fd925b1
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-02-27 23:39:55 +0900

    feat: 공지 유형에 따른 텍스트, 스타일 추가

---
+++ src/constants/notification.ts
@@ -0,0 +1,79 @@
+import { NotificationCardInfo } from '@/types';
+
+export const NOTIFICATION_TYPE: Record<number, NotificationCardInfo> = {
+  0: {
+    borderColor: '#FFC3E0',
+    alertType: '한끼팟',
+    // TODO 한끼팟 알림 메시지 백엔드에서 보내주는것인지 확인 필요
+  },
+  1: {
+    borderColor: '#FFC3E0',
+    alertType: '한끼팟',
+    // TODO 한끼팟 알림 메시지 백엔드에서 보내주는것인지 확인 필요
+  },
+  2: {
+    borderColor: '#FF8196',
+    alertType: '라운지 / 댓글',
+    alertMessage: '에 새로운 댓글이 등록되었습니다.',
+    buttonText: '게시글 보러가기',
+  },
+  3: {
+    borderColor: '#CDACFD',
+    alertType: '공지사항 / 댓글',
+    alertMessage: '에 새로운 댓글이 등록되었습니다.',
+    buttonText: '공지사항 보러가기',
+  },
+  4: {
+    borderColor: '#FFB48C',
+    alertType: '특강 / 행사',
+    alertMessage: '에 새로운 스프의 참여 신청이 있습니다.',
+    buttonText: '참여자 관리 이동',
+  },
+  5: {
+    borderColor: '#FFB48C',
+    alertType: '특강 / 행사',
+    alertMessage: '에 참여 취소가 있습니다.',
+  },
+  6: {
+    borderColor: '#FFB48C',
+    alertType: '특강 / 행사',
+    alertMessage: '신청이 승인되었습니다! 🎉',
+    buttonText: '이벤트 확인하기',
+  },
+  7: {
+    borderColor: '#FFB48C',
+    alertType: '특강 / 행사',
+    alertMessage: '신청이 반려되었습니다.',
+    buttonText: '이벤트 확인하기',
+  },
+  8: {
+    borderColor: '#00AC49',
+    alertType: '공지사항',
+    alertMessage: '새로운 공지가 등록되었습니다.',
+    buttonText: '공지사항 보러가기',
+  },
+  9: {
+    borderColor: '#FFB48C',
+    alertType: '특강 / 행사',
+    alertMessage: '이 곧 시작됩니다!⏳',
+    buttonText: 'Zoom 링크 열기',
+  },
+  10: {
+    borderColor: '#FFB48C',
+    alertType: '특강 / 행사',
+    alertMessage: `은 어떠셨나요? 💭, \n 📌 만족도 조사에 참여해 주세요!`,
+    buttonText: '설문 참여하기',
+  },
+  11: {
+    borderColor: '#81E2FF',
+    alertType: '일정 관리',
+    alertMessage: '캘린더 권한이 부여되었습니다. 📅',
+    buttonText: '일정 관리로 이동',
+  },
+  12: {
+    borderColor: '#81E2FF',
+    alertType: '일정 관리',
+    alertMessage: '캘린더 권한 부여가 필요한 매니저가 있습니다.',
+    buttonText: '캘린더 권한 부여 이동',
+  },
+};
