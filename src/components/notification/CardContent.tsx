c7de2240373a24ff5be33bb40de7eaa475f9a165
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-02-27 23:43:15 +0900

    design: 알림센터 디자인 수정, 알림 유형에 따른 스타일 구현

--- src/components/notification/CardContent.tsx
+++ src/components/notification/CardContent.tsx
@@ -1,9 +1,12 @@
 import { MouseEvent } from 'react';
 
+import { Link } from 'react-router-dom';
+
 import { useNotification } from '@/hooks/notification/useNotification';
 
 import { formatElapsedTime } from '@/utils/formatElapsedTime';
 
+import { NOTIFICATION_TYPE } from '@/constants/notification';
 import { Notification } from '@/types';
 import { BsX } from 'react-icons/bs';
 import { RiCheckFill } from 'react-icons/ri';
@@ -20,8 +23,9 @@ interface CardContentProps {
  */
 
 export default function CardContent({ notification }: CardContentProps) {
-  const { id, content, isRead } = notification;
+  const { id, content, isRead, createdAt, type, url } = notification;
   const { deleteNotification, setNotificationAsRead } = useNotification();
+  const { alertType, buttonText } = NOTIFICATION_TYPE[type];
 
   const handleDeleteNotification = (e: MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
@@ -29,29 +33,38 @@ export default function CardContent({ notification }: CardContentProps) {
   };
 
   return (
-    <div className="flex flex-col gap-[14px]">
-      <div className="flex items-center justify-between">
-        <h3 className="flex items-center gap-2">
-          <div className="size-[14px] rounded-full border bg-mainGreen" />
-          <span className="text-darkGray-active">공지사항</span>
-          <span className="font-normal text-darkGray">
-            {/* TODO 시간 - 리스폰스 데이터로 수정 */}
-            {formatElapsedTime(`2025-02-15T14:30:00Z`)}
-          </span>
-        </h3>
-        {isRead ? (
-          <button onClick={handleDeleteNotification}>
-            <BsX className="size-6 text-darkGray-hover" />
-          </button>
-        ) : (
-          <button onClick={() => setNotificationAsRead(id)}>
-            <RiCheckFill className="size-6 p-0.5 text-darkGray-hover" />
-          </button>
-        )}
+    <>
+      <div className="flex flex-col gap-[14px]">
+        <div className="flex items-center justify-between">
+          <h3 className="flex items-center gap-2">
+            <span className="text-sm text-darkGray-active">{alertType}</span>
+            <span className="text-sm font-normal text-darkGray">
+              {formatElapsedTime(createdAt)}
+            </span>
+          </h3>
+          {isRead ? (
+            <button onClick={handleDeleteNotification}>
+              <BsX className="size-6 text-darkGray-hover" />
+            </button>
+          ) : (
+            <button onClick={() => setNotificationAsRead(id)}>
+              <RiCheckFill className="size-6 p-0.5 text-darkGray-hover" />
+            </button>
+          )}
+        </div>
+        <p className="line-clamp-2 w-full overflow-hidden text-ellipsis text-start text-[#2b2b2b]">
+          {content}
+        </p>
       </div>
-      <p className="line-clamp-2 w-full overflow-hidden text-ellipsis text-start text-darkGray">
-        {content}
-      </p>
-    </div>
+
+      {buttonText && (
+        <Link
+          to={`/post/${url}`}
+          className="mt-[20px] flex items-center justify-self-end rounded-lg bg-mainGray-active px-[10px] py-2 text-white"
+        >
+          {buttonText}
+        </Link>
+      )}
+    </>
   );
 }
