eca91fc0bfe5e8bc7ad8613cfdb5eec872445158
jellie <talentforest0501@gmail.com>
2025-03-26 17:44:52 +0900

    fix: 에러 페이지 mainRoutes Suspense 추가

--- src/route/mainRoutes.tsx
+++ src/route/mainRoutes.tsx
@@ -1,4 +1,4 @@
-import { lazy } from 'react';
+import { Suspense, lazy } from 'react';
 
 import { RouteObject } from 'react-router-dom';
 
@@ -34,7 +34,11 @@ const mainRoutes: RouteObject[] = [
   {
     path: '/',
     element: <Layout type="trainee" />,
-    ErrorBoundary: () => ErrorPage({ type: 'trainee' }),
+    ErrorBoundary: () => (
+      <Suspense fallback={<div>Loading...</div>}>
+        <ErrorPage type="trainee" />
+      </Suspense>
+    ),
     children: [
       {
         index: true,
@@ -101,7 +105,11 @@ const mainRoutes: RouteObject[] = [
   {
     path: '/admin',
     element: <Layout type="admin" />,
-    ErrorBoundary: () => ErrorPage({ type: 'admin' }),
+    ErrorBoundary: () => (
+      <Suspense fallback={<div>Loading...</div>}>
+        <ErrorPage type="admin" />
+      </Suspense>
+    ),
     children: [
       {
         index: true,
