ea5a2e366bf9a380f404d1dd7e23f55e0ee8fa1e
jellie <talentforest0501@gmail.com>
2025-04-24 15:36:00 +0900

    feat: 사용자 관리 캠퍼스별 교육과정 필터

--- src/pages/admin/UserManagement.tsx
+++ src/pages/admin/UserManagement.tsx
@@ -8,6 +8,10 @@ import {
   useGetInfiniteUserList,
 } from '@/services/admin/userToManageQueries';
 import { useGetUserProfile } from '@/services/auth/authQueries';
+import {
+  useGetCampusList,
+  useGetCourseListByCampus,
+} from '@/services/campusCourse/campusCourseQueries';
 
 import {
   USER_MANAGEMENT_TAB_LIST,
@@ -45,21 +49,54 @@ export default function UserManagement() {
     handleResetFilter,
   } = useFilterData<UserManagementFilter>({ initialFilter });
 
+  const { data: userProfile } = useGetUserProfile();
+
   const { data: userList } = useGetInfiniteUserList(debouncedFilter);
 
   const { data: traineeList } = useGetInfiniteTraineeList(debouncedFilter);
+
+  const { data: campusList } = useGetCampusList();
+
+  useEffect(() => {
+    if (userProfile) {
+      handleChangeFilter({
+        courseId: userProfile?.courseList[0].courseId,
+        campusId: userProfile?.campusList[0].id,
+      });
+    }
+  }, [handleChangeFilter, userProfile]);
+
+  const campusOptionList = campusList?.map(({ id, name }) => ({ id, name }));
+
+  const selectedCampusOption =
+    campusOptionList?.find(({ id }) => id === currFilter.campusId) ||
+    campusOptionList?.[0];
+
+  const courseListByCampusData = useGetCourseListByCampus(
+    selectedCampusOption?.id ? [selectedCampusOption?.id] : [],
+  );
+
+  if (!userProfile || !campusList || !courseListByCampusData[0].data)
+    return null;
+
+  const courseOptionList = courseListByCampusData[0].data.map(
+    ({ id, title: name }) => ({ id, name }),
+  );
+
+  const selectedCourseOption =
+    courseOptionList.find(({ id }) => id === currFilter.courseId) ||
+    courseOptionList[0];
 
   const dataTypeObj = {
     'user-list': {
       labelList: userLabelList,
       data: userList,
-      gridStyle:
-        'grid grid-cols-[80px_0.8fr_1.5fr_1fr_0.8fr_2.4fr_50px] gap-x-3',
+      gridStyle: 'grid-cols-[80px_0.8fr_1.5fr_1fr_0.8fr_2.4fr_50px]',
     },
     'trainee-list': {
       labelList: traineeLabelList,
       data: traineeList,
-      gridStyle: 'grid grid-cols-[80px_0.8fr_1.5fr_1fr_2fr_50px] gap-x-3',
+      gridStyle: 'grid-cols-[80px_0.8fr_1.5fr_1fr_2fr_50px]',
     },
   };
 
@@ -67,46 +104,6 @@ export default function UserManagement() {
     dataTypeObj[(tabName ?? 'trainee-list') as UserManagementTabName];
 
   const { data, labelList, gridStyle } = dataType;
-
-  const { data: userProfile } = useGetUserProfile();
-
-  useEffect(() => {
-    if (userProfile) {
-      const { courseList, campusList } = userProfile;
-
-      handleChangeFilter({
-        courseId: courseList[0].courseId,
-        campusId: campusList[0].id,
-      });
-    }
-  }, [handleChangeFilter, userProfile]);
-
-  if (!userProfile) return null;
-  const { campusList, courseList } = userProfile;
-
-  const courseOptionList = courseList.map(course => ({
-    id: course.courseId,
-    name: course.courseTitle,
-  }));
-
-  const campusOptionList = campusList.map(campus => ({
-    id: campus.id,
-    name: campus.campusName,
-  }));
-
-  const selectedCampusOption = campusOptionList.find(
-    ({ id }) => id === currFilter.campusId,
-  ) || {
-    id: campusOptionList[0].id,
-    name: campusOptionList[0].name,
-  };
-
-  const selectedCourseOption = courseOptionList.find(
-    ({ id }) => id === currFilter.courseId,
-  ) || {
-    id: courseOptionList[0].id,
-    name: courseOptionList[0].name,
-  };
 
   return (
     <MainView className="mb-20">
@@ -119,10 +116,10 @@ export default function UserManagement() {
         tabClassName="!p-3"
       />
 
-      {userProfile && (
-        <div className="my-4 flex items-center justify-between gap-4">
+      <div className="my-4 flex items-center justify-between gap-4">
+        {campusOptionList && (
           <SingleSelectDropdown
-            defaultLabel={campusList[0].campusName || '캠퍼스 선택'}
+            defaultLabel="캠퍼스 선택"
             options={campusOptionList}
             selectedOption={selectedCampusOption}
             onChangeValue={option =>
@@ -131,9 +128,11 @@ export default function UserManagement() {
             selectBoxClassName="w-full rounded-xl border-0 justify-between items-center h-12"
             optionClassName="text-lg hover:bg-lightGray-active !py-2 pl-1 data-[selected=true]:text-black data-[selected=true]:font-bold"
           />
+        )}
 
+        {courseListByCampusData[0].data && (
           <SingleSelectDropdown
-            defaultLabel={courseList[0].courseTitle || '교육과정 선택'}
+            defaultLabel="교육과정 선택"
             options={courseOptionList}
             selectedOption={selectedCourseOption}
             onChangeValue={option =>
@@ -142,20 +141,20 @@ export default function UserManagement() {
             selectBoxClassName="w-[400px] rounded-xl border-0 justify-between items-center h-12"
             optionClassName="text-lg hover:bg-lightGray-active !py-2 pl-1 data-[selected=true]:text-black data-[selected=true]:font-bold"
           />
+        )}
 
-          <SearchInput
-            name="search"
-            value={currFilter.keyword}
-            placeholder="이름을 입력해주세요."
-            onChange={handleChangeKeyword}
-            className="ml-auto text-lg"
-            inputStyle="square"
-            resetChange={handleResetFilter}
-          />
-        </div>
-      )}
+        <SearchInput
+          name="search"
+          value={currFilter.keyword}
+          placeholder="이름을 입력해주세요."
+          onChange={handleChangeKeyword}
+          className="ml-auto text-lg"
+          inputStyle="square"
+          resetChange={handleResetFilter}
+        />
+      </div>
 
-      <header className={`${gridStyle} mb-2 mt-5 px-6`}>
+      <header className={`grid ${gridStyle} mb-2 mt-5 gap-x-3 px-6`}>
         {[...labelList, `총 ${data?.totalCounts || 0}명`].map(label => (
           <span key={label} className="text-darkGray-active">
             {label}
@@ -170,7 +169,7 @@ export default function UserManagement() {
               <UserManagementItem
                 user={user}
                 type={tabName as 'trainee-list' | 'user-list'}
-                className={`${gridStyle}`}
+                className={`grid gap-x-3 ${gridStyle}`}
               />
             </Link>
           ))}
