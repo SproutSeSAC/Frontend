5b8b7c67b1284d5d9e803beeca28e8d171601980
jellie <talentforest0501@gmail.com>
2025-08-15 15:47:46 +0900

    feat: 댓글 페이지네이션

--- src/hooks/post/useHandleComment.ts
+++ src/hooks/post/useHandleComment.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 import { useQueryClient } from '@tanstack/react-query';
 
@@ -24,7 +24,11 @@ interface UseHandleCommentProps {
   }>;
 }
 
+const PAGE_PER_NUM = 10;
+
 export const useHandleComment = ({ postId, reset }: UseHandleCommentProps) => {
+  const [currPage, setCurrPage] = useState(1);
+
   const [isEditingComment, setIsEditingComment] = useState({
     commentId: 0,
     isEditing: false,
@@ -37,6 +41,15 @@ export const useHandleComment = ({ postId, reset }: UseHandleCommentProps) => {
 
   const { data: { profileImageUrl: imgUrl } = initialUserProfile } =
     useGetUserProfile();
+
+  const movePage = (page: number) => setCurrPage(page);
+
+  const totalPage = Math.ceil(commentList.length / PAGE_PER_NUM);
+
+  const commentListPerPage = useMemo(() => {
+    const startNum = (currPage - 1) * PAGE_PER_NUM;
+    return commentList.slice(startNum, startNum + PAGE_PER_NUM);
+  }, [commentList, currPage]);
 
   const invalidateQueries = async () => {
     await queryClient.invalidateQueries({
@@ -102,6 +115,9 @@ export const useHandleComment = ({ postId, reset }: UseHandleCommentProps) => {
   const onDeleteCommentClick = async (commentId: number) => {
     try {
       await deleteComment({ commentId });
+      if (commentListPerPage.length === 1) {
+        movePage(currPage - 1);
+      }
       showToast('댓글을 삭제했어요!');
     } catch (err) {
       showToast('댓글삭제를 실패했어요');
@@ -110,6 +126,7 @@ export const useHandleComment = ({ postId, reset }: UseHandleCommentProps) => {
 
   return {
     commentList,
+    commentListPerPage,
     onSubmit,
     onEditSubmit,
     isEditingComment,
@@ -119,5 +136,8 @@ export const useHandleComment = ({ postId, reset }: UseHandleCommentProps) => {
     isPostCommentPending,
     isEditCommentPending,
     isDeleteCommentPending,
+    currPage,
+    totalPage,
+    movePage,
   };
 };
